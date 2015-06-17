65bb1e99ce23720b514d6f392bc7a2ba2cebfe98
Aidan Curtis <aidancurtis@aidans-mbp.nc.rr.com>
2015-06-17 17:35:55 -0400

    Initial Commit

---
+++ Snag'em/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Snag'em
+//
+//  Created by Aidan Curtis on 6/17/15.
+//  Copyright (c) 2015 NCSU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Snag'em/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Snag'em
+//
+//  Created by Aidan Curtis on 6/17/15.
+//  Copyright (c) 2015 NCSU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
